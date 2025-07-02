0d2f6f855a3be7ef758e981ee8287f8589ba5d7e
unknown <duracell4444@gmail.com>
2025-07-02 21:09:35 +0300

    mobile menu nav fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,7 +116,7 @@ export default function Header() {
             className="hover:underline"
             onClick={() => setMenuOpen(false)}
           >
-            Бронирование
+            Забронировать
           </Link>
           <Link
             to="/about"
